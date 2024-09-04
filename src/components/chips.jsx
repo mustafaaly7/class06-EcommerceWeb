f9d4e2ed86afae79435e6a14dc29a2dbffefbb4c
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-04 18:37:24 +0500

    categories added

--- src/components/chips.jsx
+++ src/components/chips.jsx
@@ -3,14 +3,20 @@
 
 
 
-function Chip({name}) {
+function Chip({name, onclick,chosen}) {
+  console.log(chosen);
+  
     return (
         <>
 
 
   <button
     type="button"
-    className="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-blue-700 outline-none bg-transparent hover:bg-blue-700 text-blue-700 hover:text-white transition-all duration-300"
+    onClick={onclick}
+    className={`
+    
+      px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-blue-700 outline-none bg-transparent hover:bg-blue-700 text-blue-700 hover:text-white transition-all duration-300
+       `}
   >
     {name}
   </button>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,15 +2,18 @@ import { useState } from "react"
 import { useEffect } from "react"
 import { Card } from "../components/card"
 import { Chip } from "../components/chips"
-import { data } from "autoprefixer"
 
 function Products() {
     const [categories, setCategories] = useState([])
     const [loader, setLoader] = useState(true)
     const [product, setProduct] = useState([])
-
-
+    const [searchProduct, setSearchProduct] = useState("")
+    const [selectedCategory, setSelectedCategory] = useState("All")
+    let chosenCategory
+    let url;
     useEffect(() => {
+
+
         fetch('https://dummyjson.com/products/categories')
             .then((res) => res.json())
             .then((data) => {
@@ -25,14 +28,18 @@ function Products() {
 
             })
 
-    }, [])
+    }, [selectedCategory])
 
     useEffect(() => {
+        console.log("category updated");
 
-        fetch('https://dummyjson.com/products')
+        selectedCategory == "All"
+            ? url = "https://dummyjson.com/products"
+            : url = `https://dummyjson.com/products/category/${selectedCategory}`
+        fetch(url)
             .then(res => res.json())
             .then((data) => setProduct(data.products))
-    }, [])
+    }, [selectedCategory])
 
 
 
@@ -41,53 +48,61 @@ function Products() {
 
 
         <>
-           {
-            loader ? (
-                <h3 className=" my-5 text-center text-3xl">LOADING.... </h3>
-            ):(
-                <div className=" my-5 font-[sans-serif] space-x-4 space-y-4 text-center">
-<h3 className="text-center text-3xl"> Categories</h3>
-                {categories.map((data)=>(
-                    <Chip name ={data.name} key={data.slug}/>
-                ))}
-                                </div>
-
-            )
-           }
-
-
-
-                        
-
-
-
-
-
-
-
-                        <section className="text-gray-600 body-font">
-                            <div className="container px-5 py-24 mx-auto">
-                                <div className="flex flex-wrap -m-4">
-                                    {product.map((data) => (
-
-                                        <Card title={data.title} catgeory={data.category} id={data.id} price={data.price} image={data.thumbnail} key={data.id} />
-                                    ))}
-
-
-                                </div>
-                            </div>
-                        </section>
-
-
-
-
-
-                    </>
+            {
+                loader ? (
+                    <h3 className=" my-5 text-center text-3xl">LOADING.... </h3>
+                ) : (
+                    <div className=" my-5 font-[sans-serif] space-x-4 space-y-4 text-center">
+                        <h3 className="text-center text-3xl"> Categories</h3>
+                        <Chip name={"All"} onclick={() => setSelectedCategory("All")} chosen ={chosenCategory ==categories.slug} />
+                        {categories.map((data) => (
+                            <Chip chosen ={chosenCategory == categories.slug}  name={data.name} key={data.slug} onclick={() => setSelectedCategory(data.slug)} />
+                        ))}
+                    </div>
 
                 )
+            }
 
 
-            }
-            export {
-                Products
-            }
+
+
+
+
+            <div className="flex flex-wrap w-full items-center w-full">
+                <input
+                    type="text"
+                    value={searchProduct}
+                    placeholder="Search Product Here"
+                    onChange={(e) => setSearchProduct(e.target.value)}
+                    className="border border-3  placeholder-bold placeholder-gray-500 placeholder:text-2xl text-2xl font-bold shadow xl:w-96 max-lg:w-full lg:ml-10 max-md:mt-4 max-lg:ml-4 bg-gray-100 focus:bg-transparent px-6 rounded h-11 outline-[#333] text-sm transition-all"
+                />
+            </div >
+
+
+
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                        {product.map((data) => (
+
+                            <Card title={data.title} catgeory={data.category} id={data.id} price={data.price} image={data.thumbnail} key={data.id} />
+                        ))}
+
+
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+        </>
+
+    )
+
+
+}
+export {
+    Products
+}
