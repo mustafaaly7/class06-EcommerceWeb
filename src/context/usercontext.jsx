c7810e139d9d9f420dce3e258ad603016a4e4fce
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-11 21:43:25 +0500

    not complete yet wiht onauth changed

--- src/context/usercontext.jsx
+++ src/context/usercontext.jsx
@@ -1,23 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
+import { auth, onAuthStateChanged } from "../firebase";
 
 
 export const userContext = createContext()
 
-function UserContextProvider({children}){
-const [user,setUser] = useState({
-userLogin : false,  
-    email :"",
+function UserContextProvider({ children }) {
+    const [user, setUser] = useState({
+        userLogin: false,
+        email: "",
+    })
+
+useEffect(()=>{
+onAuthStateChanged(auth,(user)=>{
+    if(user){
+        setUser({
+            userLogin : true ,
+            email :user.email
+        })
+    }
 })
 
 
-return(
-<userContext.Provider value={{user,setUser}}>
-    {children}
-</userContext.Provider>
+},[])
 
 
-)
+    return (
+        <userContext.Provider value={{ user, setUser }}>
+            {children}
+        </userContext.Provider>
+
+
+    )
 
 
 
@@ -26,6 +40,6 @@ return(
 
 }
 
-export{
+export {
     UserContextProvider
 }
