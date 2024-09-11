036dfc2ba19f59df2cd1800a11801782b235c99a
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-12 01:06:00 +0500

    Login signup completed

--- src/components/chips.jsx
+++ src/components/chips.jsx
@@ -15,7 +15,7 @@ function Chip({name, onclick,chosen}) {
     onClick={onclick}
     className={`${chosen ? "bg-blue-700 text-white" : "border border-blue-700 outline-none bg-transparent"}
     
-      px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-blue-700 outline-none bg-transparent hover:bg-blue-700 text-blue-700 hover:text-white transition-all duration-300
+      px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-blue-700 outline-none  hover:bg-blue-700 text-blue-700 hover:text-white transition-all duration-300
        `}
   >
     {name}
